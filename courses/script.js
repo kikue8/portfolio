6c15753e27a83fd8d11d834f15499e7b7370b3d8
kikue8 <82225909+kikue8@users.noreply.github.com>
2023-03-01 14:22:58 -0600

    Update script.js

--- courses/script.js
+++ courses/script.js
@@ -83,5 +83,5 @@ const closeNav = (ev) =>{
 }
 
 document.querySelector(".fa-bars").onclick = openNav;
-document.querySelector(".fa-times").onclick = closeNav;
+document.querySelector(".fa-x").onclick = closeNav;
 document.querySelector("#sidebar, .selected").onclick = closeNav;
